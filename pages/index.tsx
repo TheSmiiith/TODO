ad6b2394ca49d2c1dc98606c20b88a67174bc0fe
TheSmiiith <kovacs_attila99@outlook.hu>
2024-07-04 11:32:53 +0300

    Adjust mobile design - smaller paddings/fonts

--- pages/index.tsx
+++ pages/index.tsx
@@ -47,23 +47,23 @@ export default function Home() {
       return (
         <li
           key={todo.id}
-          className="group flex cursor-pointer items-center justify-between rounded bg-white p-4 shadow hover:bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600"
+          className="group flex cursor-pointer items-center justify-between rounded bg-white p-2 shadow hover:bg-gray-100 lg:p-4 dark:bg-gray-700 dark:hover:bg-gray-600"
           onClick={() => handleCheck(todo.id)}
         >
           <div className="flex w-full flex-row content-between items-center">
             <input
               type="checkbox"
-              className="mr-4 h-5 w-5"
+              className="mr-3 h-4 w-4 lg:mr-4 lg:h-5 lg:w-5"
               checked={todo.completed}
               readOnly
             />
             <div className={`flex flex-1 flex-col`}>
               <h3
-                className={`text-lg ${todo.completed && "text-gray-400 line-through"}`}
+                className={`text-md lg:text-lg ${todo.completed && "text-gray-400 line-through"}`}
               >
                 {todo.title}
               </h3>
-              <span className={`text-sm text-gray-400`}>
+              <span className={`text-xs text-gray-400 lg:text-sm`}>
                 {dateFormatter.format(todo.date)}
               </span>
             </div>
@@ -83,12 +83,12 @@ export default function Home() {
     <div
       className={`flex h-screen w-screen items-center justify-center overflow-hidden text-gray-950 dark:text-gray-200 ${inter.className}`}
     >
-      <div className="sm:width-8/12 flex h-4/5 w-10/12 flex-col gap-4 p-4 md:w-6/12 lg:w-5/12">
+      <div className="flex h-4/5 w-11/12 flex-col gap-4 p-4 sm:w-9/12 lg:w-6/12">
         <div className="flex flex-col gap-4 overflow-auto text-center">
           <h1 className="text-3xl font-bold">TODO Application</h1>
           <form className="flex flex-row" onSubmit={handleAdd}>
             <input
-              className="min-w-0 flex-1 rounded-l border-2 border-r-0 p-4 focus:border-blue-500 focus:outline-none dark:border-gray-600 dark:bg-gray-700"
+              className="min-w-0 flex-1 rounded-l border-2 border-r-0 p-3 focus:border-blue-500 focus:outline-none lg:p-4 dark:border-gray-600 dark:bg-gray-700"
               type="text"
               value={title}
               onChange={(e) => setTitle(e.target.value)}
@@ -102,7 +102,7 @@ export default function Home() {
             </button>
           </form>
         </div>
-        <div className="flex flex-1 flex-col overflow-auto rounded-lg bg-gray-50 p-6 shadow-lg dark:bg-gray-800">
+        <div className="flex flex-1 flex-col overflow-auto rounded-lg bg-gray-50 p-3 shadow-lg lg:p-5 dark:bg-gray-800">
           <ul className="flex flex-1 flex-col gap-2 overflow-scroll">
             {renderTodos()}
           </ul>
